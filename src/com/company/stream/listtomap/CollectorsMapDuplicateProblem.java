fed5dae58e9e8de478abc19b67aa636b41635e1b
Taner GENC <taner.genc@consultant.turkcell.com.tr>
2022-04-07 17:29:06 +0300

    ListToMap

---
+++ src/com/company/stream/listtomap/CollectorsMapDuplicateProblem.java
@@ -0,0 +1,23 @@
+package com.company.stream.listtomap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CollectorsMapDuplicateProblem {
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person(1, "Fyodor Dostoyevski", 32));
+        personList.add(new Person(2, "Lev Tolstoy", 40));
+        personList.add(new Person(3, "İvan Turgenyev", 25));
+        personList.add(new Person(4, "Nikolay Vasilyeviç Gogol", 60));
+        personList.add(new Person(5, "Maksim Gorki", 55));
+        personList.add(new Person(6, "Maksim Gorki", 55));
+
+        Map<String, Integer> personNameAgeMap = personList.stream()
+                .collect(Collectors.toMap(Person::name, Person::age));
+        System.out.println(personNameAgeMap);
+    }
+}
+//record Person(int id, String name, int age) {}
